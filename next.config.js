bc5ae75b2e7781403cbd8a79106517738931d6ce
Eyad Hajisa-i <72065224+BluEyedo@users.noreply.github.com>
2025-07-04 00:31:21 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
+const isGithubPages = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
   output: "export",
-  images: { unoptimized: true }
+  images: { unoptimized: true },
+  basePath: isGithubPages ? '/inoor_website' : '',
+  assetPrefix: isGithubPages ? '/inoor_website/' : '',
 };
 
 module.exports = nextConfig;
