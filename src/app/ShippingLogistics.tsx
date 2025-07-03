93521a693cdb85484620b13129eba8bda597df7a
Eyad Hajisa-i <72065224+BluEyedo@users.noreply.github.com>
2025-07-03 23:52:04 +0700

    update 03072025-2

--- src/app/ShippingLogistics.tsx
+++ src/app/ShippingLogistics.tsx
@@ -5,6 +5,10 @@ import ShippingIcon from "@/../public/icons/shipping.png";
 import BoxIcon from "@/../public/icons/box.png";
 import GlobeIcon from "@/../public/icons/globe.png";
 import DocumentIcon from "@/../public/icons/document.png";
+import DhlLogo from "@/../public/dhl.png";
+import AramexLogo from "@/../public/aramex.png";
+import FedexLogo from "@/../public/fedex.png";
+
 type Props = {};
 
 const ShippingLogistics = (props: Props) => {
@@ -92,6 +96,70 @@ const ShippingLogistics = (props: Props) => {
           <p>Full shipping documentation (invoices, certificates of origin)</p>
         </div>
       </motion.div>
+
+      <motion.div
+        initial={{ scale: 0 }}
+        whileInView={{ scale: 1 }}
+        viewport={{ once: true }}
+        className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold text-center mt-16 md:mb-6 mb-8 text-charcoal-500 "
+      >
+        <div className="flex flex-row ">
+          <div className="sm:w-[35%] border-b-2 border-charcoal-500" />
+          <p className="sm:w-[30%]">Our Parterns</p>
+          <div className="sm:w-[35%] border-b-2 border-charcoal-500" />
+        </div>
+      </motion.div>
+
+      <div className="grid grid-cols-3 place-content-center">
+        <motion.div
+          initial={{ scale: 0 }}
+          whileInView={{ scale: 1 }}
+          transition={{ delay: 0.2 }}
+          viewport={{ once: true }}
+          className=""
+        >
+          <Image
+            alt=""
+            src={DhlLogo}
+            width={195}
+            height={195}
+            className="w-auto"
+          />
+        </motion.div>
+        <motion.div
+          initial={{ scale: 0 }}
+          whileInView={{ scale: 1 }}
+          transition={{ delay: 0.4 }}
+          viewport={{ once: true }}
+          className=""
+        >
+          <Image
+            alt=""
+            src={FedexLogo}
+            width={195}
+            height={195}
+            className="w-auto"
+          />
+        </motion.div>
+        <motion.div
+          initial={{ scale: 0 }}
+          whileInView={{ scale: 1 }}
+          transition={{ delay: 0.6 }}
+          viewport={{ once: true }}
+          className=""
+        >
+          <Image
+            alt=""
+            src={AramexLogo}
+            width={195}
+            height={195}
+            className="w-auto"
+          />
+        </motion.div>
+        <div className="text-lg sm:text-xl md:text-2xl lg:text-3xl lg:col-span-3 justify-self-center">
+          ...and more
+        </div>
+      </div>
     </div>
   );
 };
--- src/app/shipping-logistics/page.tsx
+++ src/app/shipping-logistics/page.tsx
@@ -1,12 +1,8 @@
 "use client";
 import { motion } from "motion/react";
-import Image from "next/image";
 import React from "react";
 import ShippingLogistics from "../ShippingLogistics";
 
-import DhlLogo from "@/../public/dhl.png";
-import AramexLogo from "@/../public/aramex.png";
-import FedexLogo from "@/../public/fedex.png";
 
 type Props = {};
 
@@ -23,70 +19,6 @@ const page = (props: Props) => {
           "
       >
         <ShippingLogistics />
-
-        <motion.div
-          initial={{ scale: 0 }}
-          whileInView={{ scale: 1 }}
-          viewport={{ once: true }}
-          className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold text-center mt-16 md:mb-6 mb-8 text-charcoal-500 "
-        >
-          <div className="flex flex-row ">
-            <div className="sm:w-[35%] border-b-2 border-charcoal-500" />
-            <p className="sm:w-[30%]">Our Parterns</p>
-            <div className="sm:w-[35%] border-b-2 border-charcoal-500" />
-          </div>
-        </motion.div>
-
-        <div className="grid grid-cols-3 place-content-center">
-          <motion.div
-            initial={{ scale: 0 }}
-            whileInView={{ scale: 1 }}
-            transition={{ delay: 0.2 }}
-            viewport={{ once: true }}
-            className=""
-          >
-            <Image
-              alt=""
-              src={DhlLogo}
-              width={195}
-              height={195}
-              className="w-auto"
-            />
-          </motion.div>
-          <motion.div
-            initial={{ scale: 0 }}
-            whileInView={{ scale: 1 }}
-            transition={{ delay: 0.4 }}
-            viewport={{ once: true }}
-            className=""
-          >
-            <Image
-              alt=""
-              src={FedexLogo}
-              width={195}
-              height={195}
-              className="w-auto"
-            />
-          </motion.div>
-          <motion.div
-            initial={{ scale: 0 }}
-            whileInView={{ scale: 1 }}
-            transition={{ delay: 0.6 }}
-            viewport={{ once: true }}
-            className=""
-          >
-            <Image
-              alt=""
-              src={AramexLogo}
-              width={195}
-              height={195}
-              className="w-auto"
-            />
-          </motion.div>
-          <div className="text-lg sm:text-xl md:text-2xl lg:text-3xl lg:col-span-3 justify-self-center">
-            ...and more
-          </div>
-        </div>
       </div>
     </main>
   );
