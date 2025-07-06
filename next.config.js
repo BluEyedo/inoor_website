fa1ac58e4400c7a3a8d98d05217da2bab1705459
Eyad Hajisa-i <72065224+BluEyedo@users.noreply.github.com>
2025-07-06 13:38:56 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,28 @@
 /** @type {import('next').NextConfig} */
+const { version } = require("./package.json");
 
 const nextConfig = {
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+  generateBuildId: async () => {
+    return version;
+  },
+  reactStrictMode: false,
+  trailingSlash: true,
+  swcMinify: true,
+  modularizeImports: {
+    "@mui/icons-material": {
+      transform: "@mui/icons-material/{{member}}",
+    },
+  },
+  publicRuntimeConfig: {
+    version,
+  },
+  webpack: (config) => {
+    config.resolve.fallback = { fs: false };
+
+    return config;
+  },
+
   images: { unoptimized: true },
 };
 
